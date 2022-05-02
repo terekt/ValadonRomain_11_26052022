2b355d7d1056b2caff47a0064e56f4cede611771
Romainv78 <romainvaladon@gmail.com>
2022-05-02 13:51:47 +0200

    css

--- my-app/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import Banner from "../components/Banner/Banner";
-import Data from "../data/logements.json";
-import Card from "../components/Card/Card";
-
-function Home() {
-
-}
-
-export default Home;
---
+++ my-app/src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import Banner from "../../components/Banner/Banner";
+import Data from "../../data/logements.json";
+import Card from "../../components/Card/Card";
+import "./Home.css";
+
+function Home() {
+
+}
+
+export default Home;
