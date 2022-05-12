0c721c4a7e1c21073e99caa236f3e52b15b63cbe
Romainv78 <romainvaladon@gmail.com>
2022-05-12 14:27:27 +0200

    About page

--- my-app/src/pages/About/About.js
+++ my-app/src/pages/About/About.js
@@ -1,6 +1,22 @@
 import "./About.css";
+import Banner from "../../components/Banner/Banner";
+import Dropdown from "../../components/Dropdown/Dropdown"
 
 function About() {
+
+    return (
+        <>
+            <div className="about">
+                <Banner page="about" text="" />
+                <div className="dropdownAbout">
+                    <Dropdown data="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." title="Fiabilité" />
+                    <Dropdown data="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." title="Respect" />
+                    <Dropdown data="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." title="Service" />
+                    <Dropdown data="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." title="Responsabilité" />
+                </div>
+            </div>
+        </>
+    )
 
 }
 
