54baf0df05aa6ed3eed5a5e0702defa2e3fdb227
Romainv78 <romainvaladon@gmail.com>
2022-05-12 17:18:31 +0200

    Accueil

--- my-app/src/components/Header/Header.js
+++ my-app/src/components/Header/Header.js
@@ -9,7 +9,7 @@ function Header() {
                 <img src={Logo} alt="Kasa logo" />
                 <nav className="menu">
                     <ul>
-                        <li><NavLink className={({ isActive }) => (isActive ? 'active' : '')} to="/">Acceuil</NavLink></li>
+                        <li><NavLink className={({ isActive }) => (isActive ? 'active' : '')} to="/">Accueil</NavLink></li>
                         <li><NavLink className={({ isActive }) => (isActive ? 'active' : '')} to="/About">A Propos</NavLink></li>
                     </ul>
                 </nav>
