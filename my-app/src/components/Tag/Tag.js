49e7fb9889204f3bf100241f055e2af42f5efa10
Romainv78 <romainvaladon@gmail.com>
2022-05-12 15:31:01 +0200

    tag id fix + 404 all error

--- my-app/src/components/Tag/Tag.js
+++ my-app/src/components/Tag/Tag.js
@@ -3,7 +3,7 @@ import "./Tag.css";
 function Tag(props) {
     var tags = [];
     for (let i = 0; i < props.data.length; i++) {
-        tags.push(<div className="tag">{props.data[i]}</div>)
+        tags.push(<div className="tag"  key={i}>{props.data[i]}</div>)
     }
 
     return (
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,6 +4,7 @@ import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Housing from './pages/Housing/Housing';
+import Page404 from './pages/404/404';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import "./index.css";
@@ -17,6 +18,7 @@ root.render(
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/location/:housingId" element={<Housing />} />
+                    <Route path="*" element={<Page404 />} />
                 </Routes>
             <Footer />
         </Router>
